2c9c18a3620d8dc25cf886e5d7a5864e2390ce6a
Kaiwen Luo <luokw8@gmail.com>
2019-06-30 02:01:58 +1000

    feat: migration

--- app/Http/Controllers/Web/AuthenticationController.php
+++ app/Http/Controllers/Web/AuthenticationController.php
@@ -7,5 +7,9 @@ use App\Http\Controllers\Controller;
 
 class AuthenticationController extends Controller
 {
-    //
+    try {
+        return Socialite::with($account)->redirect();
+    } catch (\InvalidArgumentException $e) {
+        return redirect('/login');
+    }
 }
